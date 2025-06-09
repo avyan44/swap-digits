a76502342787578279851b2dfc15b15325027402
avyan44 <sumianubha@gmail.com>
2025-06-09 18:31:46 +0100

    swap

---
+++ swapr.py
@@ -0,0 +1,9 @@
+i = {"Js": "v1"}  # example value
+
+d = {
+    "MAIN": f"/images/logos/logos-{i['Js']}/logo.svg",
+    "WHITE": f"/images/logos/logos-{i['Js']}/logo-white.svg",
+    "MAIN_WITH_TEXT": f"/images/logos/logos-{i['Js']}/logo-with-text.svg"
+}
+
+print(d)
